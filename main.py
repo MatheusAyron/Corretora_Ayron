1f14e903cba12945548407b2568866830f123fcf
MatheusAyron <matheusayron12@gmail.com>
2022-02-28 14:21:40 -0300

    Ordem_correta

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv() 
 from models.papel import Papel
 from models.user import User
-from dotenv import load_dotenv
 
-load_dotenv() 
 
 user  = User.from_cpf('44444')
 papel = Papel('OIBR3')
 
 print(user)
 
-user.comprar(papel, 10)
+#user.comprar(papel, 10)
 
 print(user)
 
