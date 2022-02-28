8f8362e24bd9dccc087db271bb331c284e12efe7
MatheusAyron <matheusayron12@gmail.com>
2022-02-28 16:46:47 -0300

    Encriptografia de senha

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from models.papel import Papel
 from models.user import User
 
 
+""" dados_user = User(
+    nome = 'Matheus Ayron',
+    email= 'ttt@gmail.com',
+    senha= '123456',
+    cpf= '8888888',
+    patrimonio= 450.3
+) """
+
+#dados_user.cadastrar()
 user  = User.from_cpf('44444')
-papel = Papel('OIBR3')
+papel = Papel('AERI3')
+
+user.comprar(papel, 10)
 
 print(user)
-
-#user.comprar(papel, 10)
-
-print(user)
-
--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ import bcrypt
 import pymongo
 from models.papel import Papel, PapelEmCarteira
 from models.exceptions import SaldoInsuficiente
+from datetime import date
 
 
 class User(BaseModel):
@@ -27,6 +28,8 @@ class User(BaseModel):
     def cadastrar(self):
         """ Insere o usúario no banco de dados, tabela user, e valida se o CPF ou Email ja existem """
         
+        self.senha = bcrypt.hashpw(self.senha.encode('utf-8'), bcrypt.gensalt())
+
         try: 
             result = database.client.corretora_ayron.user.insert_one(self.dict())
         except pymongo.errors.DuplicateKeyError as e:
@@ -40,20 +43,42 @@ class User(BaseModel):
         
         self.saldo_atual -= papel.preco_atual * qtd
 
+
         # se existir esse papel na carteira, atualiza o valor
         #   self.carteira = 
         # se não existir, cria esse elemento na carteira com a qtd atual/inicial
         #   self.carteira = 
-        
+
+        for i in self.carteira:
+            if i.codigo_papel == papel.codigo_papel:
+                i.qtd         +=  qtd
+                break
+        else:                                                                                   #Caso não tenha um break
+            self.carteira.append(PapelEmCarteira(codigo_papel = papel.codigo_papel, qtd = qtd)) #Append acrescenta o valor
+
+
         database.client.corretora_ayron.user.update_one(
             {'cpf': self.cpf}, 
-            {
+            { "$set": {
                 'saldo_atual': self.saldo_atual,
-                'carteira':    self.carteira
-            }
+                'carteira':    [i.dict() for i in self.carteira ]   #para todos os iens na minha carteira eu tranformo para um dicionario, pq o mongo não sabe como salvar no banco
+            }}
         )
 
         # adicionar essa transação ao historico
+
+        database.client.corretora_ayron.historico.insert_one({
+            'cpf_user': self.cpf,
+            #'data_hora': [i.dict() for i in date.today()],
+            'papel': {
+                'codigo': papel.codigo_papel,
+                'qtd': qtd,
+                'valor_unitario': papel.preco_atual,
+                'valor_total': papel.preco_atual * qtd
+            },
+            #'qtd_apos_operacao': ,
+            'operacao': 'compra'
+        })
 
         '''
             {
@@ -69,3 +94,7 @@ class User(BaseModel):
                 'operacao': 'VENDA', # ou COMPRA
             }
         '''
+
+
+
+#criar funçõa get historico, e retorno pra nois todo o historico
